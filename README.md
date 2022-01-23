4a563a8cef74948d00346eac4e1ea1ebf28944fb
Rene Kreijveld <email@renekreijveld.nl>
2022-01-23 16:24:51 +0100

    Version 1.0.10, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# DestinyCookiePlugin
+# Destiny Cookie Plugin
+Joomla plugin to load functional, analytical and marketing scripts after the website visitor approves the use of cookies. Latest version: 1.0.10.
+
+## Installation instructions
+- Download the latest release <a href="https://github.com/renekreijveld/DestinyCookiePlugin/releases" target="_blank">here</a>.
+- Install the plugin in your Joomla 3 or Joomla 4 based website.
+- Immediately after installation you can click the link to go to the plugin configuration.
+- You can then set the settings for the Banner, Popup and the scripts.
+
+## How does the plugin work?
+Most of the functional, analytical and marketing scripts you use on your website store cookies on the computer of your website visitor. This plugin works on the concept that these scripts are only loaded when the website visitor approves the storage of functional, analytical or marketing cookies.
+
+To handle the loading af scripts after approval you can set the scripts in the plugin. You can set functional, analytical and marketing scripts. For each of these scripts you can set where the plugin needs to load the scripts.
--- script.destinycookieplugin.php
+++ script.destinycookieplugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Destiny Cookie Plugin
- * @version 1.0.9
+ * @version 1.0.10
  * @copyright Copyright (C) 2021 Destiny B.V., All rights reserved.
  * @license GNU General Public License version 3 or later; see LICENSE.txt
  * @author url: https://www.destiny.nl
@@ -31,17 +31,17 @@ class plgSystemDestinycookiepluginInstallerScript
         $year = date('Y');
 		if ($lang->getTag() == 'nl-NL')
 		{
-			$message = "<h3><b>Destiny Cookie Plugin 1.0.9 - Joomla 4 versie</b></h3>
+			$message = "<h3><b>Destiny Cookie Plugin 1.0.10</b></h3>
 			Vergeet niet om na deze installatie de plugin instellingen te controleren en de plugin te publiceren.<br>
 			<a href='index.php?option=com_plugins&view=plugins&filter[search]=destiny' style='text-decoration:underline;'>Klik hier om naar de plugininstellingen te gaan</a>.<br>
-			Versie 1.0.9, copyright &copy; 2021 - $year Destiny B.V., alle rechten voorbehouden. <a href='https://www.destiny.nl' target='_blank'><b>www.destiny.nl</b></a><br>";
+			Versie 1.0.10, copyright &copy; 2021 - $year Destiny B.V., alle rechten voorbehouden. <a href='https://www.destiny.nl' target='_blank'><b>www.destiny.nl</b></a><br>";
 		}
 		else
 		{
-			$message = "<h3><b>Destiny Cookie Plugin 1.0.9 - Joomla 4 version</b></h3>
+			$message = "<h3><b>Destiny Cookie Plugin 1.0.10</b></h3>
 			Don't forget to check the plugin settings and to publish the plugin after this installation.<br>
 			<a href='index.php?option=com_plugins&view=plugins&filter[search]=destiny' style='text-decoration:underline;'>Click here to go to the plugin settings</a>.<br>
-			Version 1.0.9, copyright &copy; 2021 - $year Destiny B.V., all rights reserved. <a href='https://www.destiny.nl' target='_blank'><b>www.destiny.nl</b></a><br>";
+			Version 1.0.10, copyright &copy; 2021 - $year Destiny B.V., all rights reserved. <a href='https://www.destiny.nl' target='_blank'><b>www.destiny.nl</b></a><br>";
 		}
 		$app = Factory::getApplication();
 		Factory::getApplication()->enqueueMessage($message, 'info');
